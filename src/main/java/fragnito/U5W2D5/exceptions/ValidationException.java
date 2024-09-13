fdb5bd5ca7c90931ebb1f0a8ebf7dd32d50d3da3
C043 <mariofragnitoph@gmail.com>
2024-09-13 11:33:16 +0200

    Adds ExceptionHandler

--- src/main/java/fragnito/U5W2D5/exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package fragnito.U5W2D5.exceptions;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
